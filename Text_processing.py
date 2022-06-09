cc300095cde4de0186f65c8d69c28d2e63e51ac3
Richard Hu <84014189+Richard0307@users.noreply.github.com>
2022-06-10 00:00:35 +0100

    day2

--- Text_processing.py
+++ Text_processing.py
@@ -1,7 +1,25 @@
-import binascii
-text = "Simply Easy Learning"
+from array import array
+import numpy as np
+import pandas as pd
+import jieba 
+import jieba.analyse
+import encodings 
 
-# Converting binary to ascii
-data_b2a = binascii.b2a_uu(text)
-print("**Binary to Ascii ** \n")
-print (data_b2a)
+a = "哎，鹅，听说你超级喜欢小游戏的！你是吗？"
+b = jieba.lcut(a)
+stopword = []
+with open('C:\\Users\\Richard\\Desktop\\COM3110 代码\\test\\stopword.txt','r',encoding='utf-8') as f:
+    for line in f.readlines():
+        l = line.strip()
+        if l == '\\n': # 换行符
+            l = '\n'
+        if l == '\\u3000':# 制表符
+            l = '\u3000'
+        
+        stopword.append(l)
+
+x = np.array(b)
+y = np.array(stopword)
+z = x[~np.in1d(x,y)]
+k = [i for i in z if len(i)>1]
+print(k)
